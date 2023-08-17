a43901beb7b9e55966d5f21de7c0d3e0e6100844
griguk77 <griguk77@gmail.com>
2023-08-17 19:59:11 +0300

    Use custom builder

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.dependencyinjectionstart.example2.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dependencyinjectionstart.R
-import com.example.dependencyinjectionstart.example1.Activity
-import com.example.dependencyinjectionstart.example2.di.ContextModule
 import com.example.dependencyinjectionstart.example2.di.DaggerApplicationComponent
 import javax.inject.Inject
 
@@ -15,7 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     private val component by lazy {
         DaggerApplicationComponent.builder()
-            .contextModule(ContextModule(this))
+            .context(application)
+            .timeMillis(System.currentTimeMillis())
             .build()
     }
 
