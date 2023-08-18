16a84fbff9185b11c856b88a8d91a1431b618cb1
griguk77 <griguk77@gmail.com>
2023-08-18 03:25:50 +0300

    Use custom scopes

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
@@ -10,7 +10,7 @@ import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
-@Singleton
+@ApplicationScope
 @Component(modules = [DataModule::class, DomainModule::class])
 interface ApplicationComponent {
 
