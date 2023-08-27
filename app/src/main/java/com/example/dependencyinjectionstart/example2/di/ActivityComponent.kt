4a6a11e50136fdf69d53d1fa7a2a93888237aa5c
griguk77 <griguk77@gmail.com>
2023-08-28 00:02:29 +0300

    Add qualifiers

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/ActivityComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/ActivityComponent.kt
@@ -15,7 +15,8 @@ interface ActivityComponent {
     @Subcomponent.Factory
     interface Factory {
         fun create(
-            @BindsInstance id: String
+            @BindsInstance @IdQualifier id: String,
+            @BindsInstance @NameQualifier name: String
         ): ActivityComponent
     }
 }
